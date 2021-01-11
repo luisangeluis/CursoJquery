993eca9ab88f231cc8a999f6d4237f120f74641a
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-01-11 13:56:56 -0600

    Se revisa metodo stop de animaciones

---
+++ js/metodoStop.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	//alert('hola');
+
+	$('.caja').show(800,function(){
+		//alert('hola');
+	});
+	/*
+	$('#ejecutar').on({
+
+		click: function(){
+
+			$('.caja').animate({
+
+				marginLeft: '500px'
+			},4000);
+
+			$('.caja').animate({
+
+				marginLeft: '0'
+			},4000);
+		
+		}
+
+
+	});
+
+
+	$('#stop').on({
+
+		click:function(){
+				//$(elemento).stop(limpiarRestoAnimaciones, saltar al final);
+			$('.caja').stop(true,true);
+		}
+
+	})
+	*/
+});
