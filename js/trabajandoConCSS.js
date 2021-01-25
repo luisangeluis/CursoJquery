1001f611b5e966507a2ed7e28ea7b8baa1d831ba
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-01-25 14:10:00 -0600

    Se revisa el video trabajando con css

---
+++ js/trabajandoConCSS.js
@@ -0,0 +1,10 @@
+$(document).ready(function principal(){
+	$('#boton').on({
+		click:function(){
+			//$(this).addClass('naranja');
+			//$(this).toggleClass('naranja');
+			$(this).removeClass('ejemplo-boton');
+		}
+	});
+	// body...
+});
